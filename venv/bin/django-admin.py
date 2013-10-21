159548adb86aff3509dce2f4807f23f85acbd82f
Research <research@ubuntu.(none)>
2013-10-20 22:45:30 -0700

    Udemy course - Python and Python Django for Beginners 2

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/research/Dev/DjangoUdemy/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'Django==1.5.4','django-admin.py'
+__requires__ = 'Django==1.5.4'
+import pkg_resources
+pkg_resources.run_script('Django==1.5.4', 'django-admin.py')
